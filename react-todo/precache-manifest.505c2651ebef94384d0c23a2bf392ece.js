f9685401cd6ecc2ddabff8528ef95500f0beec3b
i333859 <tsvetelin.pantev@sap.com>
2020-03-02 15:43:26 +0200

    react-todo

---
+++ react-todo/precache-manifest.505c2651ebef94384d0c23a2bf392ece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531e70f05b696b852e2b0f0407d5af49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9baf8b187ba805ab365",
+    "url": "/static/js/2.0ee18458.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0ee18458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668bfdc9fe3e6a8aedb0",
+    "url": "/static/js/main.f1608beb.chunk.js"
+  },
+  {
+    "revision": "b12d7dbb37ac1cd37421",
+    "url": "/static/js/runtime-main.97e8b550.js"
+  }
+]);
