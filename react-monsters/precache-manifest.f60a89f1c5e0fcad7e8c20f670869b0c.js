933d3b0620e27504d1e9a391879b9a847c7f2583
i333859 <tsvetelin.pantev@sap.com>
2020-04-04 19:59:55 +0300

    react monsters

---
+++ react-monsters/precache-manifest.f60a89f1c5e0fcad7e8c20f670869b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c926476efdb50a0b786857f0d05b61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "445fbd9e0da96eb35cbb",
+    "url": "/static/css/main.067cb5ed.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "445fbd9e0da96eb35cbb",
+    "url": "/static/js/main.3dce96d1.chunk.js"
+  },
+  {
+    "revision": "81e1d083e26926c78a0f",
+    "url": "/static/js/runtime-main.82e3bd2a.js"
+  }
+]);
