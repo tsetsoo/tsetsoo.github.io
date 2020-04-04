b132e96fc115e52f4806b2f5bd2980229e06c0cf
i333859 <tsvetelin.pantev@sap.com>
2020-04-04 20:23:25 +0300

    gh-pages build

---
+++ react-monsters/precache-manifest.ddef289287aa8c6e5fc8f7dec10d6555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6378942dc8f75e59b5a79e14096a065c",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "7a87e531e1a64dca4cfc",
+    "url": "/react-monsters/static/css/main.82259b55.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a87e531e1a64dca4cfc",
+    "url": "/react-monsters/static/js/main.39fabf45.chunk.js"
+  },
+  {
+    "revision": "10a25d6082fe69f22468",
+    "url": "/react-monsters/static/js/runtime-main.578e257e.js"
+  }
+]);
--- react-monsters/service-worker.js
+++ react-monsters/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc4e07f6cebac74906b62317dba55d40.js"
+  "/react-monsters/precache-manifest.ddef289287aa8c6e5fc8f7dec10d6555.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-monsters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
